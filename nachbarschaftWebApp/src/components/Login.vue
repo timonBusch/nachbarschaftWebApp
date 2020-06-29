fbab63b1a37cb88ec4b31777661a8d6793962c25
Lukas_von_der_Heide <lukas.von_der_heide@fh-bielefeld.de>
2020-06-29 11:42:05 +0200

    Login mit Cookies, Nutzungs- und Datenschutzbedingungen

--- nachbarschaftWebApp/src/components/Login.vue
+++ nachbarschaftWebApp/src/components/Login.vue
@@ -7,8 +7,13 @@
                 </div>
                 <div class="card-body">
                     <b-form v-if="show">
-                        <div v-if="this.wrong" class="alert alert-danger">
-                            <strong>E-Mail-Adresse oder Password falsch!</strong> Überprüfen Sie Ihre Eingaben.
+                        <div v-if="this.wrong || this.cookie_wrong || this.datenschutz_wrong" class="alert alert-danger">
+                            <div v-if="this.wrong">
+                                <strong>E-Mail-Adresse oder Password falsch!</strong> Überprüfen Sie Ihre Eingaben.
+                            </div>
+                            <div v-if="this.cookie_wrong || this.datenschutz_wrong">
+                                <strong>Cookie, Datenschutz und Nutzerbedingungen</strong> Sie müssen die Cookies zulassen und die Nutzungs- und Datenschutzbedinungn akzeptieren.
+                            </div>
                         </div>
                         <b-form-group
                                 id="input-group-1"
@@ -33,16 +38,25 @@
                                     v-model="form.password"
                                     type="password"
                                     required
-
                             ></b-form-input>
                         </b-form-group>
 
                         <p>Noch kein Konto? <router-link to="/register">Registrieren</router-link></p>
 
                         <b-form-group id="input-group-4">
+
                             <b-form-checkbox-group v-model="form.checked" id="checkboxes-4">
-                                <b-form-checkbox value="me">Angemeldet bleiben</b-form-checkbox>
+                                <b-form-checkbox value="checked_in">Angemeldet bleiben</b-form-checkbox>
                             </b-form-checkbox-group>
+
+                            <b-form-checkbox-group v-model="form.cookies" id="checkboxes-4">
+                                <b-form-checkbox value="checked_in">Hier mit stimme ich den Cookies zu.</b-form-checkbox>
+                            </b-form-checkbox-group>
+
+                            <b-form-checkbox-group v-model="form.datenschutz" id="checkboxes-4">
+                                <b-form-checkbox value="checked_in">Hier mit stimme ich den Nutzungs- und Datenschutzbedinungen zu.</b-form-checkbox>
+                            </b-form-checkbox-group>
+
                         </b-form-group>
 
                         <b-button @click.prevent="loginUser()" variant="primary">Login</b-button>
@@ -64,38 +78,53 @@
                 form: {
                     email: '',
                     password: '',
-                    checked: []
+                    checked: [],
+                    cookies: [],
+                    datenschutz: []
                 },
                 show: true,
                 msg: '',
-                wrong: false
+                wrong: false,
+                cookie_wrong: false,
+                datenschutz_wrong: false
             }
         },
         methods: {
             ...mapActions('login', ["login", "userLogin"]),
             async loginUser() {
                 try {
-                    const credentials = {
-                        username: this.form.email,
-                        password: this.form.password
-                    };
+                        this.cookie_wrong = false;
+                        this.datenschutz_wrong = false;
+                        console.log(this.form.cookies);
+                        console.log(this.form.datenschutz);
+                        const credentials = {
+                            username: this.form.email,
+                            password: this.form.password
+                        };
 
-                    const response = await AuthService.login(credentials);
+                        const response = await AuthService.login(credentials);
 
-                    this.msg = response.msg;
+                        this.msg = response.msg;
 
-                    const token = response.jwt;
+                        const token = response.jwt;
 
-                    this.login(token);
+                        this.login(token);
 
-                    const user = await axios.get( 'http://85.214.106.187:8080/nachbarschaftshilfe-0.0.1/benutzer/email?email='
-                        + credentials.username)
-                        .then(response => response.data);
+                        const user = await axios.get('http://85.214.106.187:8080/nachbarschaftshilfe-0.0.1/benutzer/email?email='
+                            + credentials.username)
+                            .then(response => response.data);
 
-                    this.userLogin(user);
-                    this.$router.push('/')
-                    this.wrong = false;
 
+                        this.wrong = false;
+                        if(this.form.datenschutz.length !== 0 && this.form.cookies.length !== 0) {
+                            this.cookie_wrong = false;
+                            this.datenschutz_wrong = false;
+                            this.userLogin(user);
+                            this.$router.push('/')
+                        } else {
+                            this.cookie_wrong = true;
+                            this.datenschutz_wrong = true;
+                        }
                 } catch (error) {
 
                     this.wrong = true;
